479b14b6f215cea1a3966f0b21015b1c295414b9
Lindy Crain <lindylcrain@gmail.com>
2016-09-28 16:26:49 -0600

    styling homepage

---
+++ app/components/portfolio/portfolio.js
@@ -0,0 +1,15 @@
+; (function () {
+
+    angular.module('rosefox')
+        .component('portfolioComponent', {
+            templateUrl: 'app/components/portfolio/portfolio.html',
+            controller: PortfolioController
+        })
+
+    function PortfolioController() {
+        var $ctrl = this;
+
+    }
+
+
+} ());
