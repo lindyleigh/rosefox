ab6b431c44f226a7878bbc176f79fc88518b5c92
Lindy Crain <lindylcrain@gmail.com>
2016-09-30 23:23:35 -0600

    added opacity to hamburger menu open

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -11,10 +11,12 @@
 
         $ctrl.openMenu= function() {
             angular.element(document.getElementById("hamburger").style.width = "250px");
+            angular.element(document.body.style.backgroundColor = "rgba(0,0,0,0.4)");
         }
 
         $ctrl.closeMenu= function() {
             angular.element(document.getElementById("hamburger").style.width = "0");
+            angular.element(document.body.style.backgroundColor = "white");
 }
 
     }
